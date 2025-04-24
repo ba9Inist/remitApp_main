3c7a695e4272f6b18a2fdb7d3c9d67d8cce2e90f
Golubev Egor <ged@mosremit.ru>
2025-04-24 16:25:13 +0300

    Initial Commit

---
+++ remitApp_main/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  remitApp_main
+//
+//  Created by Егор Голубев on 24.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
